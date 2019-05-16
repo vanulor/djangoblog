1f7fede47eb1f1c9a5404f2462071fed1fa37e39
Brian Nisonger <vanulor@gmail.com>
2019-05-16 01:40:51 -0700

    Added detail view

--- blogging/tests.py
+++ blogging/tests.py
@@ -4,24 +4,26 @@ from blogging.models import Post, Category
 import datetime
 from django.utils.timezone import utc
 
+
 class FrontEndTestCase(TestCase):
     """test views provided in the front-end"""
-    fixtures = ['blogging_test_fixture.json', ]
+    fixtures = [
+        'blogging_test_fixture.json',
+    ]
 
     def setUp(self):
         self.now = datetime.datetime.utcnow().replace(tzinfo=utc)
         self.timedelta = datetime.timedelta(15)
         author = User.objects.get(pk=1)
         for count in range(1, 11):
-            post = Post(title="Post %d Title" % count,
-                        text="foo",
-                        author=author)
+            post = Post(
+                title="Post %d Title" % count, text="foo", author=author)
             if count < 6:
                 # publish the first five posts
                 pubdate = self.now - self.timedelta * count
                 post.published_date = pubdate
             post.save()
-            
+
     def test_list_only_published(self):
         resp = self.client.get('/')
         # the content of the rendered response is always a bytestring
@@ -32,13 +34,28 @@ class FrontEndTestCase(TestCase):
             if count < 6:
                 self.assertContains(resp, title, count=1)
             else:
-                self.assertNotContains(resp, title)        
+                self.assertNotContains(resp, title)
+
+    def test_details_only_published(self):
+        for count in range(1, 11):
+            title = "Post %d Title" % count
+            post = Post.objects.get(title=title)
+            resp = self.client.get('/posts/%d/' % post.pk)
+            if count < 6:
+                self.assertEqual(resp.status_code, 200)
+                self.assertContains(resp, title)
+            else:
+                self.assertEqual(resp.status_code, 404)
+
 
 class PostTestCase(TestCase):
-    fixtures = ['blogging_test_fixture.json', ]
+    fixtures = [
+        'blogging_test_fixture.json',
+    ]
+
     def setUp(self):
         self.user = User.objects.get(pk=1)
-        
+
     def test_string_representation(self):
         expected = "This is a title"
         p1 = Post(title=expected)
--- blogging/views.py
+++ blogging/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
 
+
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
     if args:
@@ -12,11 +13,20 @@ def stub_view(request, *args, **kwargs):
         body += "Kwargs:\n"
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
-    
+
+
 def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
+    published = Post.objects.exclude(published_date=None)
     posts = published.order_by('-published_date')
-    template = loader.get_template('blogging/list.html')
     context = {'posts': posts}
-    body = template.render(context)
-    return HttpResponse(body, content_type="text/html")    
+    return render(request, 'blogging\list.html', context)
+
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
