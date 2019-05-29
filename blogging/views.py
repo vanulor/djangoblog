8819f10a54e2b3ac3e0efc6f133821a336f9ddd9
Brian Nisonger <vanulor@gmail.com>
2019-05-29 09:59:57 -0700

    Fixed slash in program

--- blogging/views.py
+++ blogging/views.py
@@ -22,7 +22,7 @@ def list_view(request):
     published = Post.objects.exclude(published_date=None)
     posts = published.order_by('-published_date')
     context = {'posts': posts}
-    return render(request, 'blogging\list.html', context)
+    return render(request, 'blogging/list.html', context)
 
 
 def detail_view(request, post_id):
